8db2d151f3c3b9e582ae792877a5322dc6bca88e
Ashish pal <91552546+Ashish4910@users.noreply.github.com>
2024-04-05 09:52:43 +0530

    Update product.service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -30,3 +30,121 @@ interface GetResponse {
   }
 }
 
+
+// =========>> Json data is in this format ====================> 
+
+
+//   {
+// "_embedded": {
+// "products": [
+// {
+// "sku": "BOOK-TECH-1001",
+// "name": "Become a Guru in JavaScript",
+// "description": "Learn JavaScript at your own pace. The author explains how the technology works in easy-to-understand language. This book includes working examples that you can apply to your own projects. Purchase the book and get started today!",
+// "unitPrice": 20.99,
+// "imageUrl": "assets/images/products/books/book-luv2code-1001.png",
+// "active": true,
+// "unitsInStock": 100,
+// "dateCreated": "2024-02-14T17:42:10.000+00:00",
+// "lastUpdated": null,
+// "_links": {
+// "self": {
+// "href": "http://localhost:8080/api/products/2"
+// },
+// "product": {
+// "href": "http://localhost:8080/api/products/2"
+// },
+// "category": {
+// "href": "http://localhost:8080/api/products/2/category"
+// }
+// }
+// },
+// {
+// "sku": "BOOK-TECH-1002",
+// "name": "Exploring Vue.js",
+// "description": "Learn Vue.js at your own pace. The author explains how the technology works in easy-to-understand language. This book includes working examples that you can apply to your own projects. Purchase the book and get started today!",
+// "unitPrice": 14.99,
+// "imageUrl": "assets/images/products/books/book-luv2code-1002.png",
+// "active": true,
+// "unitsInStock": 100,
+// "dateCreated": "2024-02-14T17:42:10.000+00:00",
+// "lastUpdated": null,
+// "_links": {
+// "self": {
+// "href": "http://localhost:8080/api/products/3"
+// },
+// "product": {
+// "href": "http://localhost:8080/api/products/3"
+// },
+// "category": {
+// "href": "http://localhost:8080/api/products/3/category"
+// }
+// }
+// },
+// {
+// "sku": "BOOK-TECH-1003",
+// "name": "Advanced Techniques in Big Data",
+// "description": "Learn Big Data at your own pace. The author explains how the technology works in easy-to-understand language. This book includes working examples that you can apply to your own projects. Purchase the book and get started today!",
+// "unitPrice": 13.99,
+// "imageUrl": "assets/images/products/books/book-luv2code-1003.png",
+// "active": true,
+// "unitsInStock": 100,
+// "dateCreated": "2024-02-14T17:42:10.000+00:00",
+// "lastUpdated": null,
+// "_links": {
+// "self": {
+// "href": "http://localhost:8080/api/products/4"
+// },
+// "product": {
+// "href": "http://localhost:8080/api/products/4"
+// },
+// "category": {
+// "href": "http://localhost:8080/api/products/4/category"
+// }
+// }
+// },
+// {
+// "sku": "BOOK-TECH-1004",
+// "name": "Crash Course in Big Data",
+// "description": "Learn Big Data at your own pace. The author explains how the technology works in easy-to-understand language. This book includes working examples that you can apply to your own projects. Purchase the book and get started today!",
+// "unitPrice": 18.99,
+// "imageUrl": "assets/images/products/books/book-luv2code-1004.png",
+// "active": true,
+// "unitsInStock": 100,
+// "dateCreated": "2024-02-14T17:42:10.000+00:00",
+// "lastUpdated": null,
+// "_links": {
+// "self": {
+// "href": "http://localhost:8080/api/products/5"
+// },
+// "product": {
+// "href": "http://localhost:8080/api/products/5"
+// },
+// "category": {
+// "href": "http://localhost:8080/api/products/5/category"
+// }
+// }
+// },
+// {
+// "sku": "BOOK-TECH-1005",
+// "name": "JavaScript Cookbook",
+// "description": "Learn JavaScript at your own pace. The author explains how the technology works in easy-to-understand language. This book includes working examples that you can apply to your own projects. Purchase the book and get started today!",
+// "unitPrice": 23.99,
+// "imageUrl": "assets/images/products/books/book-luv2code-1005.png",
+// "active": true,
+// "unitsInStock": 100,
+// "dateCreated": "2024-02-14T17:42:10.000+00:00",
+// "lastUpdated": null,
+// "_links": {
+// "self": {
+// "href": "http://localhost:8080/api/products/6"
+// },
+// "product": {
+// "href": "http://localhost:8080/api/products/6"
+// },
+// "category": {
+// "href": "http://localhost:8080/api/products/6/category"
+// }
+// }
+// }
+//   }
